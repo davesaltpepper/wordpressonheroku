720679b30c7beb05720c75d628dd1481716f9a8b
dave <david@add>
2012-05-25 17:07:37 -0400

    changed username password in the config sample file

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'ae232c2a06796a');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '85c91bef');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
