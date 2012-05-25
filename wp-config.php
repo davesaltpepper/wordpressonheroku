9d0f4565eafab1175b724c417c7f5d60d0988a58
dave <david@add>
2012-05-25 17:52:09 -0400

    overwrote folder, changed persmissions

--- wp-config.php
+++ wp-config.php
@@ -19,26 +19,29 @@ if(file_exists( dirname(_FILE_).'/wp-config_local.php')){
 
 	include( dirname(_FILE_).'/wp-config_local.php');
 }else{
-$db = parse_url($_ENV["CLEARDB_DATABASE_URL"]);)
-
-/** The name of the database for WordPress */
-define('DB_NAME', trim($db["path"],"/"));
-
-/** MySQL database username */
-define('DB_USER', $db["user"]);
-
-/** MySQL database password */
-define('DB_PASSWORD', $db["pass"]);
-
-/** MySQL hostname */
-define('DB_HOST', $db["host"]);
+	$db = parse_url($_ENV["CLEARDB_DATABASE_URL"]);)
+	
+	/** The name of the database for WordPress */
+	define('DB_NAME', trim($db["path"],"/"));
+	
+	/** MySQL database username */
+	define('DB_USER', $db["user"]);
+	
+	/** MySQL database password */
+	define('DB_PASSWORD', $db["pass"]);
+	
+	/** MySQL hostname */
+	define('DB_HOST', $db["host"]);
+	
+	
+}
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-}
+	define('DB_CHARSET', 'utf8');
+	
+	/** The Database Collate type. Don't change this if in doubt. */
+	define('DB_COLLATE', '');
+	
 /**#@+
  * Authentication Unique Keys and Salts.
  *
